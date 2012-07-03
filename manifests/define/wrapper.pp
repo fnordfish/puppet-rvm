bb2301b76f26db7187eee09036ee668c021b88cf
Robert Schulze <robert@dotless.de>
2012-07-03 09:11:11 +0200

    adds support for rvm gemsets when creating a rvm wrapper. (plain copy from rvm::define::gem)

--- manifests/define/wrapper.pp
+++ manifests/define/wrapper.pp
@@ -1,7 +1,8 @@
 define rvm::define::wrapper(
   $ensure = 'present',
   $ruby_version,
-  $prefix
+  $prefix,
+  $gemset = '',
 ) {
   Exec {
     path    => '/usr/local/rvm/bin:/bin:/sbin:/usr/bin:/usr/sbin',
@@ -9,17 +10,25 @@ define rvm::define::wrapper(
   $rvm_source = "source /usr/local/rvm/scripts/rvm"
   $wrapper_exists = "bash -c '${rvm_source} ; rvm use ${ruby_version} ; which ${prefix}_${name}'"
 
+  if $gemset == '' {
+    $rvm_depency = "install-ruby-${ruby_version}"
+    $rubyset_version = $ruby_version
+  } else {
+    $rvm_depency = "rvm-gemset-create-${gemset}-${ruby_version}"
+    $rubyset_version = "${ruby_version}@${gemset}"
+  }
+
   if $ensure == 'present' {
-    exec { "rvm-alias-create-${name}-${ruby_version}":
-      command => "bash -c '${rvm_source} ; rvm use ${ruby_version} ; rvm wrapper ${ruby_version} ${prefix} ${name}'",
+    exec { "rvm-alias-create-${name}-${rubyset_version}":
+      command => "bash -c '${rvm_source} ; rvm use ${rubyset_version} ; rvm wrapper ${rubyset_version} ${prefix} ${name}'",
       unless => $wrapper_exists,
-      require => [Class['rvm'], Exec["install-ruby-${ruby_version}"]],
+      require => [Class['rvm'], Exec[$rvm_depency]],
     }
   } elsif $ensure == 'absent' {
-    exec { "rvm-gemset-delete-${name}-${ruby_version}":
-      command => "bash -c '${rvm_source} ; rvm use ${ruby_version} ; rm `which ${prefix}_${name}`'",
+    exec { "rvm-gemset-delete-${name}-${rubyset_version}":
+      command => "bash -c '${rvm_source} ; rvm use ${rubyset_version} ; rm `which ${prefix}_${name}`'",
       onlyif => $wrapper_exists,
-      require => [Class['rvm'], Exec["install-ruby-${ruby_version}"]],
+      require => [Class['rvm'], Exec[$rvm_depency]],
     }
   }
 }
